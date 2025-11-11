dbcf5738670de58ce3e37bb97920b537e24a951b
tarunawahyudi <wahyuditaruna97@gmail.com>
2025-11-12 03:21:47 +0700

    disable cors

--- src/app/core/middleware/cors.middleware.ts
+++ src/app/core/middleware/cors.middleware.ts
@@ -1,15 +1,5 @@
 import cors from "@elysiajs/cors"
-import config from "@core/config"
-import { Elysia } from 'elysia'
 
 export const corsMiddleware = () => {
-  const noopPlugin = new Elysia({ name: "noop-cors" })
-
-  if (!config.cors.enabled) {
-    return noopPlugin
-  }
-
-  return cors({
-    origin: '*'
-  })
+  return cors()
 }
